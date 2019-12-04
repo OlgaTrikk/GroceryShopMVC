49121c29a9c6d00c5d2e8b1fa5e902d440e78a34
Olga Trikk <prymnesium13@gmail.com>
2019-12-04 12:32:38 +0200

    - ShoppingCartsController - Adding item to cart - Item list on ShoppingCart details view

--- GroceryShopMVC/Controllers/FoodsController.cs
+++ GroceryShopMVC/Controllers/FoodsController.cs
@@ -18,7 +18,8 @@ namespace GroceryShopMVC.Controllers
         // GET: Foods
         public ActionResult Index()
         {
-            return View(db.Foods.ToList());
+			List<Food> foods = db.Foods.OrderBy(x => x.Name).ToList();
+			return View(foods);
         }
 
         // GET: Foods/Details/5
@@ -36,86 +37,27 @@ namespace GroceryShopMVC.Controllers
             return View(food);
         }
 
-        // GET: Foods/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Foods/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Price")] Food food)
-        {
-            if (ModelState.IsValid)
-            {
-                food.Id = Guid.NewGuid();
-                db.Foods.Add(food);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(food);
-        }
-
-        // GET: Foods/Edit/5
-        public ActionResult Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Food food = db.Foods.Find(id);
-            if (food == null)
-            {
-                return HttpNotFound();
-            }
-            return View(food);
-        }
-
-        // POST: Foods/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Price")] Food food)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(food).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(food);
-        }
-
-        // GET: Foods/Delete/5
-        public ActionResult Delete(Guid? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Food food = db.Foods.Find(id);
-            if (food == null)
-            {
-                return HttpNotFound();
-            }
-            return View(food);
-        }
-
-        // POST: Foods/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
-        {
-            Food food = db.Foods.Find(id);
-            db.Foods.Remove(food);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        public ActionResult AddToCart(Guid? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Food food = db.Foods.Find(id);
+			if (food == null)
+			{
+				return HttpNotFound();
+			}
+			var shoppingCart = db.ShoppingCarts.OrderByDescending(x => x.DateCreated).FirstOrDefault();
+			if (shoppingCart == null)
+			{
+				shoppingCart = db.ShoppingCarts.Add(new ShoppingCart());
+			}
+			shoppingCart.AddToCart(food);
+			db.SaveChanges();
+			//return RedirectToAction("Index", "ShoppingCarts");
+			return RedirectToAction("Details", "ShoppingCarts", new { id = shoppingCart.Id });
+		}
 
         protected override void Dispose(bool disposing)
         {
---
+++ GroceryShopMVC/Controllers/ShoppingCartsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GroceryShopMVC.DatabaseContext;
+using GroceryShopMVC.Models;
+
+namespace GroceryShopMVC.Controllers
+{
+    public class ShoppingCartsController : Controller
+    {
+        private GroceryShopDbContext db = new GroceryShopDbContext();
+
+        // GET: ShoppingCarts
+        public ActionResult Index()
+        {
+            return View(db.ShoppingCarts.OrderByDescending(x => x.DateCreated).ToList());
+        }
+
+        // GET: ShoppingCarts/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShoppingCarts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Sum,DateCreated")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                shoppingCart.Id = Guid.NewGuid();
+                db.ShoppingCarts.Add(shoppingCart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Sum,DateCreated")] ShoppingCart shoppingCart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shoppingCart).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shoppingCart);
+        }
+
+        // GET: ShoppingCarts/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            if (shoppingCart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shoppingCart);
+        }
+
+        // POST: ShoppingCarts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            ShoppingCart shoppingCart = db.ShoppingCarts.Find(id);
+            db.ShoppingCarts.Remove(shoppingCart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
